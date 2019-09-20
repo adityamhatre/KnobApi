a277cab85d49e508452d015b033c475f9532c2bc
Aditya Mhatre <aditya.r.mhatre@gmail.com>
2019-09-19 21:12:46 -0500

    added edge cases

--- src/main/java/com/adityamhatre/knobapi/RestController.kt
+++ src/main/java/com/adityamhatre/knobapi/RestController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/knobapi")
 class RestController {
 
+    private var lastPosition = ""
     private val motorStepAngle = 1.8
 
     private val halfStepSequence =
@@ -27,12 +28,18 @@ class RestController {
 
     private var isTurning = false
 
-    private val failedJson = mapOf("success" to false, "errorReason" to "Knob is turning")
+    private val failedKnobTurningJson = mapOf("success" to false, "errorReason" to "Knob is turning")
+
+    private val failedAlreadyInLockedPositionJson = mapOf("success" to false, "errorReason" to "Knob is already locked")
+    private val failedAlreadyInUnlockedPositionJson =
+        mapOf("success" to false, "errorReason" to "Knob is already unlocked")
+
     private val successfulJson = mapOf("success" to true, "errorReason" to null)
 
     @RequestMapping("/unlock")
     fun lock(): Map<String, Any?> {
-        if (isTurning) return failedJson
+        if (lastPosition == "UNLOCKED") return failedAlreadyInUnlockedPositionJson
+        if (isTurning) return failedKnobTurningJson
         isTurning = !isTurning
         try {
             for (i in 0 until getStepsForAngle(angle = 600)) {
@@ -46,6 +53,8 @@ class RestController {
                     Thread.sleep(1)
                 }
             }
+            lastPosition = "UNLOCKED"
+            clearMotor()
         } catch (ex: Exception) {
             return mapOf("success" to false, "errorReason" to ex.toString())
         }
@@ -55,7 +64,8 @@ class RestController {
 
     @RequestMapping("/lock")
     fun unlock(): Map<String, Any?> {
-        if (isTurning) return failedJson
+        if (lastPosition == "LOCKED") return failedAlreadyInLockedPositionJson
+        if (isTurning) return failedKnobTurningJson
         isTurning = !isTurning
         try {
             for (i in 0 until getStepsForAngle(angle = 600)) {
@@ -69,11 +79,17 @@ class RestController {
                     Thread.sleep(1)
                 }
             }
+            lastPosition = "LOCKED"
+            clearMotor()
         } catch (ex: Exception) {
             return mapOf("success" to false, "errorReason" to ex.toString())
         }
         isTurning = !isTurning
         return successfulJson
+    }
+
+    private fun clearMotor() {
+        Application.provisionedPins.forEach { GPIOProvider.instance.set(it, 0) }
     }
 
     private fun getStepsForAngle(angle: Int): Int {
